9a0dd438a66afdca5d0c93932dca2772b93e7079
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-13 11:41:45 -0500

    added loading icon and put movies in html on fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const $ = require('jquery');
+
 /**
  * es6 modules and imports
  */
@@ -9,12 +11,27 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
+$('#movies').html("loading...");
+
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  $('#movies').html('Here are all the movies: ');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('#movies').append(`id#${id} - ${title} - rating: ${rating}`)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+// $('#loading').hide();
+// $('#loading').(function(){
+//   $(this).show();
+//   //console.log('shown');
+// });
+$("#loading").ajaxStop(function(){
+  $(this).hide();
+});
+
