0121910168a32b94d0ce869de4462e846f0f7816
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-16 16:54:45 -0500

    after functionallity, added cards and styling

--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ sayHello('World');
  */
 const {getMovies, postMovie, editMovie, deleteMovie } = require('./api.js');
 
-$('#movies').html("loading...");
 
 
 //This prints existing movie JSON file
 getMovies().then((movies) => {
   console.log('Here are all the existing movies:');
-  $('#movies').html('Here are all the movies: ');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
-    $('#movies').append(`<div id="${id}">id#${id} - ${title} - rating: ${rating} <h5>Edit</h5></div>`)
+    $('#movies').append(`<div id="${id}" class="col-sm-6">
+        <div class="card" style="width: 18rem;">
+         <div class="card-body">
+              <h5 class="card-title">Movie: #${id} - ${title}</h5>
+             <p class="card-text">rating: ${rating}</p>
+         </div>
+        </div>
+        </div>`)
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  // alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
 
@@ -38,7 +43,7 @@ $("#loading").ajaxStop(function(){
 
 
 //Adds new movies data to JSON file
-$('#blue').click(function () {
+$('#add').click(function () {
 
   postMovie({
     "title": $('#movieTitle').val(),
@@ -48,7 +53,12 @@ $('#blue').click(function () {
     $('#movies').empty();
     movie.forEach(({title, rating, id}) => {
       console.log(` id#${id} - ${title} - ${rating}`);
-      $('#movies').append(`<div> id#${id} - ${title} - ${rating} <h5>Edit</h5></div>`)
+      $('#movies').append(`<div> <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Movie: ${id} - ${title} </h5>
+        <p class="card-text"> Rating:${rating}</p>
+    </div>
+</div></div>`)
     });
   }).catch((error) => {
     alert('Something\'s wrong with the G Diffuser.\nCheck the console for details.');
@@ -70,7 +80,12 @@ $("#update").click(function(){
     $("#movies").empty();
     movie.forEach(({title, rating, id}) => {
       console.log(`id# ${id} - ${title} - ${rating} `);
-      $('#movies').append(`<div> id#${id} - ${title} - ${rating} <h5>Edit</h5></div>`)
+      $('#movies').append(`<div> <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Movie: ${id} - ${title} </h5>
+        <p class="card-text"> Rating: ${rating}</p>
+    </div>
+</div></div>`)
     });
   }).catch((error) => {
     alert('Something\'s wrong with the G Diffuser.\nCheck the console for details.')
@@ -90,7 +105,12 @@ $("#delete").click(function() {
       $('#movies').empty();
     movies.forEach(({title, rating, id}) => {
       console.log(`id# ${id} - ${title} - ${rating}.`);
-      $('#movies').append(`<div> id#${id} - ${title} - ${rating} <h5>Edit</h5></div>`)
+      $('#movies').append(`<div> <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Movie: ${id} - ${title}</h5>
+        <p class="card-text"> Rating: ${rating}</p>
+    </div>
+</div></div>`)
     });
   }).catch((error) => {
     alert('Something\'s wrong with the G Diffuser.\nCheck the console for details.')
